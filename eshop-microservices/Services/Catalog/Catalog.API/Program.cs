00f65a75841af3d53a006e300b087c29ee47c1d2
RsuEduard <rusu00eduard@proton.me>
2024-08-23 15:07:05 +0300

    feat: set ports for microservice

--- eshop-microservices/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to container.
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
 
 app.Run();
